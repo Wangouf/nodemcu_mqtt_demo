5da23a4f94643b56d46b799f27f00e22edd44119
Wangouf <wanghso@foxmail.com>
2021-08-10 12:32:41 +0800

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include "PubSubClient.h"
+#include "DHT.h"
+#include "ArduinoJson.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+const char topic[] = "OL9IA22UUE/esp8266/data";
+const char ssid[] = "Redmi K40";
+const char pwd[] = "password";
+const char mqtt_server[] = "OL9IA22UUE.iotcloud.tencentdevices.com";
+const char name[] = "esp8266";
+const char username[] = "OL9IA22UUEesp8266;12010126;OQK5V;1664560867";
+const char userpwd[] = "0caee5543d57408f38ee096fa09e661660d4361e0039fcaa89ac88ef85881b5f;hmacsha256";
+WiFiClient espClient;
+PubSubClient client(espClient);
+DHT dht(DHTPIN, DHTTYPE);
+void connect() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid,pwd);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+    }
+}
+void reconnect() {
+    while (!client.connected()) {
+        if (client.connect(name,username,userpwd)) {
+            client.publish(topic, "hello world");
+        } else {
+            delay(5000);
+        }
+    }
+}
+void setup() {
+    Serial.begin(9600);
+    dht.begin();
+    connect();
+    client.setServer(mqtt_server,1883);
+    client.connect(name,username,userpwd);
+}
+
+void loop() {
+    if (!client.connected()) {
+        reconnect();
+    }
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    DynamicJsonDocument doc(50);
+    doc["Temperature"] = t;
+    doc["Humidity"] = h;
+    String output;//e.g. {"Temperature":30.4,"Humidity":74}
+    serializeJson(doc,output);
+    Serial.println(output.c_str());
+    client.publish(topic,output.c_str());
+    delay(1000*30);
+}
